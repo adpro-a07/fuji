92d549c4cee28b5a4ddf874dc34673e83b8ecc8a
ryuun1corn <yuda.arif.prasojo@gmail.com>
2025-04-09 15:40:29 +0700

    fix: modify code quality issues in landing page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
 import { Metadata } from "next"
-import { Button } from "components/Button/Button"
-
-import { LP_GRID_ITEMS } from "lp-items"
 
 export const metadata: Metadata = {
   title: "Next.js Enterprise Boilerplate",
@@ -22,16 +19,14 @@ export const metadata: Metadata = {
 
 export default function Web() {
   return (
-    <>
-      <section className="bg-white dark:bg-gray-900">
-        <div className="mx-auto grid max-w-(--breakpoint-xl) px-4 py-8 text-center lg:py-16">
-          <div className="mx-auto place-self-center">
-            <h1 className="mb-4 max-w-2xl text-4xl leading-none font-extrabold tracking-tight md:text-5xl xl:text-6xl dark:text-white">
-              Hello from Adpro-A07! This is Fuji!
-            </h1>
-          </div>
+    <section className="bg-white dark:bg-gray-900">
+      <div className="mx-auto grid max-w-(--breakpoint-xl) px-4 py-8 text-center lg:py-16">
+        <div className="mx-auto place-self-center">
+          <h1 className="mb-4 max-w-2xl text-4xl leading-none font-extrabold tracking-tight md:text-5xl xl:text-6xl dark:text-white">
+            Hello from Adpro-A07! This is Fuji!
+          </h1>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   )
 }
