df6596b7123fb0858db44e33bd766c10df512f6a
Faridhsbi <mhmmd.faridhsbi@gmail.com>
2025-05-24 17:11:38 +0700

    fix: update UpdateCouponPageModule to accept couponId prop instead of params object

--- app/admin/coupons/[couponId]/edit/page.tsx
+++ app/admin/coupons/[couponId]/edit/page.tsx
@@ -1,30 +1,8 @@
-import { notFound } from "next/navigation"
 import React from "react"
-import { get } from "@/components/utils/customFetch/serverFetchClients"
-import { CouponResponseInterface } from "@/modules/ManageCouponsPageModule/interface"
-import UpdateCouponForm from "@/modules/UpdateCouponPageModule/module-elements/UpdateCouponForm"
+import UpdateCouponPageModule from "@/modules/UpdateCouponPageModule"
 
-export default async function Page({ params }: { params: { couponId: string } }) {
-  const { couponId } = params
+export default async function page(props: { params: Promise<{ couponId: string }> }) {
+  const { couponId } = await props.params
 
-  const response = await get<CouponResponseInterface>(`/api/v1/coupons/${couponId}`, {
-    isAuthorized: true,
-  })
-
-  if (!response.success || !response.data) {
-    notFound()
-  }
-
-  const coupon = {
-    code: response.data.code,
-    discountAmount: response.data.discountAmount,
-    maxUsage: response.data.maxUsage,
-    validUntil: response.data.validUntil,
-  }
-
-  return (
-    <div className="pt-16">
-      <UpdateCouponForm coupon={coupon} couponId={couponId} />
-    </div>
-  )
+  return <UpdateCouponPageModule params={{ couponId }} />
 }
