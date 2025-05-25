b2d4ff9bd2999392f1a3b94fc73465efa6683729
Danniel-Ang <danniel@ui.ac.id>
2025-05-25 19:02:55 +0700

    Fixing format

--- app/technician-report/customer-technician-reports/page.tsx
+++ app/technician-report/customer-technician-reports/page.tsx
@@ -1,10 +1,6 @@
 import React from "react"
 import CustomerTechnicianReportModule from "@/modules/CustomerTechnicianReportPageModule"
 
-export default function Page({
-                               searchParams,
-                             }: {
-  searchParams: { status?: string }
-}) {
+export default function Page({ searchParams }: { searchParams: { status?: string } }) {
   return <CustomerTechnicianReportModule status={searchParams.status} />
 }
