9e8d4c057674bda1f4a24a7a064d6cb8099a31af
Faridhsbi <mhmmd.faridhsbi@gmail.com>
2025-05-25 14:28:19 +0700

    refactor: delete typo

--- src/modules/AdminTechnicianReportListModule/sections/MainTechnicianReportListSection.tsx
+++ src/modules/AdminTechnicianReportListModule/sections/MainTechnicianReportListSection.tsx
@@ -26,7 +26,7 @@ export default function MainTechnicianReportListSection({
   })
 
   return (
-    <div className="mxmin-h-screen bg-gray-50 transition-colors duration-300 dark:bg-gray-900">
+    <div className="min-h-screen bg-gray-50 transition-colors duration-300 dark:bg-gray-900">
       <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
         {/* Header */}
         <div className="mb-8">
