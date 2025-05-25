97a1ba3c71bcfd0244488bb7f8e1ea115adca172
ArditheusIH <arditheus.immanuel@gmail.com>
2025-05-25 18:19:54 +0700

    fix formating and lint

--- app/admin/payment-methods/[paymentMethodId]/page.tsx
+++ app/admin/payment-methods/[paymentMethodId]/page.tsx
@@ -1,12 +1,8 @@
 import React from "react"
 import PaymentMethodDetailPageModule from "@/modules/PaymentMethodDetailPageModule"
 
-export default async function PaymentMethodDetailPage({
-                                                        params
-                                                      }: {
-  params: Promise<{ paymentMethodId: string }>
-}) {
-  const { paymentMethodId } = await params;
+export default async function PaymentMethodDetailPage({ params }: { params: Promise<{ paymentMethodId: string }> }) {
+  const { paymentMethodId } = await params
 
   return <PaymentMethodDetailPageModule paymentMethodId={paymentMethodId} />
 }
